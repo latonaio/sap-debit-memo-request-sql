4f0f86ad442ea7cbce0c29b2c703ab50e9468bd1
Yurino Oga <yurino.o@latona.io>
2022-02-05 00:16:39 +0900

    release

--- sap-debit-memo-request-sql-header-partner-data.sql
+++ sap-debit-memo-request-sql-header-partner-data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `sap_credit_memo_request_header_partner_data`
+CREATE TABLE `sap_debit_memo_request_header_partner_data`
 (
   `DebitMemoRequest`             varchar(10) NOT NULL,
   `PartnerFunction`              varchar(2) NOT NULL,
